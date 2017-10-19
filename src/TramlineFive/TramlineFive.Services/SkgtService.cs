656f4b964ec8821b07c67e2db46451a167ee7859
betrakiss <angelin.nedelchev@outlook.com>
2017-10-19 10:58:58 +0300

    Add services project.

---
+++ src/TramlineFive/TramlineFive.Services/SkgtService.cs
@@ -0,0 +1,19 @@
+﻿using SkgtService.Models;
+using SkgtService.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TramlineFive.Services
+{
+    public static class SkgtManager
+    {
+        public static ISkgtParser Parser { get; } = new DesktopSkgtParser();
+        public static Line SelectedLine { get; set; }
+        public static event EventHandler<IEnumerable<string>> OnTimingsReceived;
+        public static void SendTimings(object sender, IEnumerable<string> timings)
+        {
+            OnTimingsReceived?.Invoke(sender, timings);
+        }
+    }
+}
