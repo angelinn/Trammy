520587d68c1e978125134ef999dc9490b1e6a542
angelinn <angelin.nedelchev@outlook.com>
2017-11-22 11:14:50 +0200

    Change service to use the new API instead of parsing.

---
+++ src/TramlineFive/SkgtService/Parsers/ArrivalsService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkgtService.Parsers
+{
+    public class ArrivalsService
+    {
+        private readonly HttpClient client = new HttpClient();
+        private const string ARRIVALS_API_URL = @"https://api-arrivals.sofiatraffic.bg/api/v1/arrivals";
+        private const string OPERA_USER_AGENT = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36 OPR/48.0.2685.35";
+
+        public ArrivalsService()
+        {
+            client.DefaultRequestHeaders.Add("User-Agent", OPERA_USER_AGENT);
+        }
+
+        public async Task GetByStopCodeAsync(string stopCode)
+        {
+            HttpResponseMessage message = await client.GetAsync($"{ARRIVALS_API_URL}/{stopCode}/");
+            string json = await message.Content.ReadAsStringAsync();
+        }
+    }
+}
--- src/TramlineFive/SkgtService/SkgtManager.cs
+++ src/TramlineFive/SkgtService/SkgtManager.cs
@@ -8,8 +8,6 @@ namespace SkgtService
 {
     public static class SkgtManager
     {
-        public static IStopCodeParser StopCodeParser { get; } = new StopCodeParser();
-        public static ILineParser LineParser { get; } = new LineParser();
         public static SkgtObject SelectedLine { get; set; }
         public static event EventHandler<IEnumerable<string>> OnTimingsReceived;
         public static void SendTimings(object sender, IEnumerable<string> timings)
