27f32be6693c6c4aa5dc2b61b239776621b82a83
angelinn <angelin.nedelchev@outlook.com>
2017-11-23 17:45:41 +0200

    Add Stop model.

---
+++ src/TramlineFive/SkgtService/Models/Stop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkgtService.Models
+{
+    public class Stop
+    {
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public string Code { get; set; }
+        public string Id { get; set; }
+        public string PublicName { get; set; }
+        public string PublicNameEn { get; set; }
+    }
+}
