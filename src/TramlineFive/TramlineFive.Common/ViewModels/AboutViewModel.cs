454ea6e7eb1d2825af37542351c02795d7e658df
angelinn <angelin.nedelchev@outlook.com>
2017-11-25 12:37:49 +0200

    Add about page UI.

---
+++ src/TramlineFive/TramlineFive.Common/ViewModels/AboutViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TramlineFive.Common.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        private string version;
+        public string Version
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(version))
+                    version = ApplicationService.GetVersion();
+
+                return version;
+            }
+            set
+            {
+                version = value;
+            }
+        }
+    }
+}
--- src/TramlineFive/TramlineFive.Common/ViewModels/Locator/ViewModelLocator.cs
+++ src/TramlineFive/TramlineFive.Common/ViewModels/Locator/ViewModelLocator.cs
@@ -12,11 +12,13 @@ namespace TramlineFive.Common.ViewModels.Locator
             SimpleIoc.Default.Register<VirtualTablesViewModel>();
             SimpleIoc.Default.Register<HistoryViewModel>();
             SimpleIoc.Default.Register<HamburgerViewModel>();
+            SimpleIoc.Default.Register<AboutViewModel>();
         }
 
         public VirtualTablesViewModel VirtualTablesViewModel => SimpleIoc.Default.GetInstance<VirtualTablesViewModel>();
         public HistoryViewModel HistoryViewModel => SimpleIoc.Default.GetInstance<HistoryViewModel>();
         public HamburgerViewModel HamburgerViewModel => SimpleIoc.Default.GetInstance<HamburgerViewModel>();
+        public AboutViewModel AboutViewModel => SimpleIoc.Default.GetInstance<AboutViewModel>();
 
     }
 }
