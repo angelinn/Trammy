d826f451ae915b173367c4f302884ec89e070db7
angelinn <angelin.nedelchev@outlook.com>
2018-02-20 01:49:44 +0200

    Add StopsLoader.

---
+++ src/TramlineFive/SkgtService/Models/Locations/StopLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkgtService.Models.Locations
+{
+    public class StopLocation
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public string PublicName { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ src/TramlineFive/SkgtService/StopsLoader.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using SkgtService.Models.Locations;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SkgtService
+{
+    public class StopsLoader
+    {
+        public List<StopLocation> LoadStops(Stream stream)
+        {
+            string json = String.Empty;
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                json = reader.ReadToEnd();
+            }
+            return JsonConvert.DeserializeObject<List<StopLocation>>(json);
+        }
+    }
+}
