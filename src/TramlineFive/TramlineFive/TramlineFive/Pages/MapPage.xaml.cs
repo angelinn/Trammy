6bdd00d7b91aff78302c4297f89435ff4b9ed807
angelinn <angelin.nedelchev@outlook.com>
2018-02-25 00:59:45 +0200

    Port visualization logic using maps ui.

--- src/TramlineFive/TramlineFive/TramlineFive/Pages/MapPage.xaml.cs
+++ src/TramlineFive/TramlineFive/TramlineFive/Pages/MapPage.xaml.cs
@@ -1,6 +1,12 @@
-﻿using Mapsui.Projection;
+﻿using GalaSoft.MvvmLight.Command;
+using Mapsui.Geometries;
+using Mapsui.Layers;
+using Mapsui.Projection;
+using Mapsui.Providers;
+using Mapsui.Styles;
 using Mapsui.Utilities;
 using SkgtService;
+using SkgtService.Models.Locations;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -19,81 +25,105 @@ namespace TramlineFive.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
+        private SymbolStyle pinStyle;
+        private List<Feature> features;
         public MapPage()
         {
             InitializeComponent();
 
             var mapControl = new MapsUIView();
             mapControl.NativeMap.Layers.Add(HumanitarianTileServer.CreateTileLayer());
-            var centerOfSofia = new Point(42.6977, 23.3219);
+            mapControl.NativeMap.Info += NativeMap_Info;
+            LoadPinStyles();
+
+            var centerOfSofia = new Mapsui.Geometries.Point(42.6977, 23.3219);
             var sphericalMercatorCoordinate = SphericalMercator.FromLonLat(centerOfSofia.Y, centerOfSofia.X);
             mapControl.NativeMap.NavigateTo(sphericalMercatorCoordinate);
             mapControl.NativeMap.NavigateTo(mapControl.NativeMap.Resolutions[14]);
+            mapControl.NativeMap.Layers.Add(LoadStops());
 
             grid.Children.Add(mapControl);
         }
 
-        protected override async void OnAppearing()
+        private void NativeMap_Info(object sender, Mapsui.UI.InfoEventArgs e)
         {
-            //if (initialized)
-            //    return;
+            foreach (var l in features)
+            {
+                StopLocation location = l["stopObject"] as StopLocation;
 
-            //initialized = true;
+                var point = new Mapsui.Geometries.Point(location.Lon, location.Lat);
+                var local = SphericalMercator.FromLonLat(point.X, point.Y);
+                var difference = e.WorldPosition - local;
 
-            //await Task.Run(() =>
-            //{
-            //    MyMap = new Map();
-            //    MyMap.MapType = MapType.Street;
-
-            //    MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
-            //new Position(42.6977, 23.3219), Distance.FromMiles(0.3)));
-
-            //    Assembly assembly = typeof(MapPage).GetTypeInfo().Assembly;
-            //    Stream stream = assembly.GetManifestResourceStream("TramlineFive.stops.json");
-            //    foreach (var location in new StopsLoader().LoadStops(stream))
-            //    {
-            //        var position = new Position(location.Lat, location.Lon); // Latitude, Longitude
-            //        var pin = new Pin
-            //        {
-            //            Type = PinType.Place,
-            //            Position = position,
-            //            Label = location.PublicName,
-            //            Address = location.Code,
-            //            IsVisible = false
-            //        };
-            //        MyMap.Pins.Add(pin);
-            //    }
-
-            //    IPermissionService permissionService = DependencyService.Get<IPermissionService>();
-
-            //    if (permissionService.HasLocationPermissions())
-            //        MyMap.MyLocationEnabled = true;
-
-            //    MyMap.CameraIdled += MyMap_CameraIdled;
-            //    Device.BeginInvokeOnMainThread(() =>
-            //    {
-            //        grid.Children.RemoveAt(0);
-            //        grid.Children.Add(MyMap);
-            //    });
-            //});
+                if (Math.Abs(difference.X) < 500 && Math.Abs(difference.Y) < 500)
+                {
+                    SymbolStyle style = l.Styles.First() as SymbolStyle;
+                    style.Enabled = true;
+                }
+            }
+            (sender as Mapsui.Map).ViewChanged(true);
         }
 
-        //private void MyMap_CameraIdled(object sender, CameraIdledEventArgs e)
-        //{
-        //    if (e.Position.Zoom < 15)
-        //    {
-        //        foreach (Pin pin in MyMap.Pins)
-        //            pin.IsVisible = false;
-        //    }
-        //    else
-        //    {
-        //        foreach (Pin pin in MyMap.Pins)
-        //        {
-        //            Position difference = e.Position.Target - pin.Position;
-        //            pin.IsVisible = (Math.Abs(difference.Latitude) < 0.01 && Math.Abs(difference.Longitude) < 0.01);
-        //        }
-        //    }
-        //}
+        private void Viewport_ViewportChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(e.PropertyName);
+        }
+
+        private ILayer LoadStops()
+        {
+            Assembly assembly = typeof(MapPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("TramlineFive.stops.json");
+
+            features = new List<Feature>();
+
+            List<StopLocation> stops = new StopsLoader().LoadStops(stream);
+            foreach (var location in stops)
+            {
+                var centerOfSofia = new Mapsui.Geometries.Point(location.Lon, location.Lat);
+                var sphericalMercatorCoordinate = SphericalMercator.FromLonLat(centerOfSofia.X, centerOfSofia.Y);
+
+                Feature feature = new Feature
+                {
+                    Geometry = sphericalMercatorCoordinate,
+                    Styles = new List<IStyle> { new SymbolStyle
+                    {
+                        Enabled = pinStyle.Enabled,
+                        BitmapId = pinStyle.BitmapId
+                    }
+                    }
+                };
+
+                feature["stopObject"] = location;
+                features.Add(feature);
+            }
+
+            return new Layer
+            {
+                Name = "Stops layer",
+                DataSource = new MemoryProvider(features),
+                Style = null
+            };
+        }
+
+        private void LoadPinStyles()
+        {
+            Assembly assembly = typeof(MapPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("TramlineFive.pin.png");
+            foreach (string name in assembly.GetManifestResourceNames())
+                System.Diagnostics.Debug.WriteLine(name);
+
+            var bitmapId = BitmapRegistry.Instance.Register(stream);
+
+            var centerOfSofia = new Mapsui.Geometries.Point(23.3219, 42.6977);
+            var sphericalMercatorCoordinate = SphericalMercator.FromLonLat(centerOfSofia.X, centerOfSofia.Y);
+            // LoadStops();
+            pinStyle = new SymbolStyle
+            {
+                BitmapId = bitmapId,
+                Enabled = false
+            };
+        }
+
     }
 }
 
