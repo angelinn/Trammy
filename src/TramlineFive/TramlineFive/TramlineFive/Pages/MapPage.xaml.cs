d42849b2a1634c4f472f67a25715a4b80cb2ce5a
angelinn <angelin.nedelchev@outlook.com>
2018-02-24 19:48:26 +0200

    Remove GoogleMaps.

--- src/TramlineFive/TramlineFive/TramlineFive/Pages/MapPage.xaml.cs
+++ src/TramlineFive/TramlineFive/TramlineFive/Pages/MapPage.xaml.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using TramlineFive.Services;
 using Xamarin.Forms;
-using Xamarin.Forms.GoogleMaps;
 using Xamarin.Forms.Xaml;
 
 namespace TramlineFive.Pages
@@ -19,50 +18,69 @@ namespace TramlineFive.Pages
         public MapPage()
         {
             InitializeComponent();
-
-            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
-                    new Position(42.6977, 23.3219), Distance.FromMiles(0.3)));
-
-            Assembly assembly = typeof(MapPage).GetTypeInfo().Assembly;
-            Stream stream = assembly.GetManifestResourceStream("TramlineFive.stops.json");
-            foreach (var location in new StopsLoader().LoadStops(stream))
-            {
-                var position = new Position(location.Lat, location.Lon); // Latitude, Longitude
-                var pin = new Pin
-                {
-                    Type = PinType.Place,
-                    Position = position,
-                    Label = location.PublicName,
-                    Address = location.Code,
-                    IsVisible = false
-                };
-                MyMap.Pins.Add(pin);
-            }
-
-            IPermissionService permissionService = DependencyService.Get<IPermissionService>();
-
-            if (permissionService.HasLocationPermissions())
-                MyMap.MyLocationEnabled = true;
-
-            MyMap.CameraIdled += MyMap_CameraIdled;
         }
 
-        private void MyMap_CameraIdled(object sender, CameraIdledEventArgs e)
+        protected override async void OnAppearing()
         {
-            if (e.Position.Zoom < 15)
-            {
-                foreach (Pin pin in MyMap.Pins)
-                    pin.IsVisible = false;
-            }
-            else
-            {
-                foreach (Pin pin in MyMap.Pins)
-                {
-                    Position difference = e.Position.Target - pin.Position;
-                    pin.IsVisible = (Math.Abs(difference.Latitude) < 0.01 && Math.Abs(difference.Longitude) < 0.01);
-                }
-            }
+            //if (initialized)
+            //    return;
+
+            //initialized = true;
+
+            //await Task.Run(() =>
+            //{
+            //    MyMap = new Map();
+            //    MyMap.MapType = MapType.Street;
+
+            //    MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
+            //new Position(42.6977, 23.3219), Distance.FromMiles(0.3)));
+
+            //    Assembly assembly = typeof(MapPage).GetTypeInfo().Assembly;
+            //    Stream stream = assembly.GetManifestResourceStream("TramlineFive.stops.json");
+            //    foreach (var location in new StopsLoader().LoadStops(stream))
+            //    {
+            //        var position = new Position(location.Lat, location.Lon); // Latitude, Longitude
+            //        var pin = new Pin
+            //        {
+            //            Type = PinType.Place,
+            //            Position = position,
+            //            Label = location.PublicName,
+            //            Address = location.Code,
+            //            IsVisible = false
+            //        };
+            //        MyMap.Pins.Add(pin);
+            //    }
+
+            //    IPermissionService permissionService = DependencyService.Get<IPermissionService>();
+
+            //    if (permissionService.HasLocationPermissions())
+            //        MyMap.MyLocationEnabled = true;
+
+            //    MyMap.CameraIdled += MyMap_CameraIdled;
+            //    Device.BeginInvokeOnMainThread(() =>
+            //    {
+            //        grid.Children.RemoveAt(0);
+            //        grid.Children.Add(MyMap);
+            //    });
+            //});
         }
+
+        //private void MyMap_CameraIdled(object sender, CameraIdledEventArgs e)
+        //{
+        //    if (e.Position.Zoom < 15)
+        //    {
+        //        foreach (Pin pin in MyMap.Pins)
+        //            pin.IsVisible = false;
+        //    }
+        //    else
+        //    {
+        //        foreach (Pin pin in MyMap.Pins)
+        //        {
+        //            Position difference = e.Position.Target - pin.Position;
+        //            pin.IsVisible = (Math.Abs(difference.Latitude) < 0.01 && Math.Abs(difference.Longitude) < 0.01);
+        //        }
+        //    }
+        //}
     }
 }
 
