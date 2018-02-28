f0c225d06f58a5a765e7b799f93e5c596dc4b92c
angelinn <angelin.nedelchev@outlook.com>
2018-02-28 23:54:33 +0200

    Dynamically get virtual tables page index.

--- src/TramlineFive/TramlineFive/TramlineFive/Services/InteractionService.cs
+++ src/TramlineFive/TramlineFive/TramlineFive/Services/InteractionService.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TramlineFive.Common.Services;
+using TramlineFive.Pages;
 using Xamarin.Forms;
 
 namespace TramlineFive.Services
 {
     public class InteractionService : IInteractionService
     {
+        public int VirtualTablesIndex
+        {
+            get
+            {
+                MasterDetailPage main = Application.Current.MainPage as MasterDetailPage;
+                TabbedPage tabbed = (main.Detail as NavigationPage).CurrentPage as TabbedPage;
+
+                return tabbed.Children.IndexOf(tabbed.Children.First(p => p.GetType() == typeof(VirtualTablesPage)));
+            }
+        }
+
         public async Task<bool> DisplayAlertAsync(string title, string message, string ok, string cancel)
         {
             if (!String.IsNullOrEmpty(cancel))
