fe0dce0056bf96aab2e5ff35d5e650f7601ea494
angelinn <angelin.nedelchev@outlook.com>
2018-03-21 23:17:17 +0200

    Port pages and buttons to MVVM.

--- src/TramlineFive/TramlineFive.Common/ViewModels/MainViewModel.cs
+++ src/TramlineFive/TramlineFive.Common/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using GalaSoft.MvvmLight.Ioc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows.Input;
 using TramlineFive.Common.Messages;
@@ -12,19 +13,48 @@ namespace TramlineFive.Common.ViewModels
     {
         public ICommand SearchCommand { get; private set; }
         public ICommand CancelSearchCommand { get; private set; }
+        public ICommand ChangeViewCommand { get; private set; }
+
+        private Dictionary<string, bool> pages = new Dictionary<string, bool>
+        {
+            { "Map", true },
+            { "Search", false },
+            { "Settings", false },
+            { "Favourites", false },
+            { "History", false }
+        };
 
         public MainViewModel()
         {
             SearchCommand = new RelayCommand(() => ActivateSearch());
             CancelSearchCommand = new RelayCommand(() => IsSearchVisible = false);
+            ChangeViewCommand = new RelayCommand<string>((p) => ChangeView(p));
+
+            RaisePropertyChanged("IsSearchVisible");
+            RaisePropertyChanged("IsMapVisible");
+            RaisePropertyChanged("IsSettingsVisible");
+            RaisePropertyChanged("IsFavouritesVisible");
+            RaisePropertyChanged("IsHistoryVisible");
+        }
+
+        private void ChangeView(string view)
+        {
+            foreach (string key in pages.Keys.ToList())
+                pages[key] = key == view;
+
+            RaisePropertyChanged("IsSearchVisible");
+            RaisePropertyChanged("IsMapVisible");
+            RaisePropertyChanged("IsSettingsVisible");
+            RaisePropertyChanged("IsFavouritesVisible");
+            RaisePropertyChanged("IsHistoryVisible");
         }
 
         private void ActivateSearch()
         {
-            if (!isSearchVisible)
+            if (!IsSearchVisible)
             {
                 IsSearchVisible = true;
-                InteractionService.ChangeTab(InteractionService.VirtualTablesIndex);
+                ChangeView("Search");
                 MessengerInstance.Send(new FocusSearchMessage());
             }
             else
@@ -50,17 +80,68 @@ namespace TramlineFive.Common.ViewModels
                 RaisePropertyChanged();
             }
         }
-
-        private bool isSearchVisible;
+        
         public bool IsSearchVisible
         {
             get
             {
-                return isSearchVisible;
+                return pages["Search"];
             }
             set
             {
-                isSearchVisible = value;
+                pages["Search"] = value;
+                RaisePropertyChanged();
+            }
+        }
+        
+        public bool IsMapVisible
+        {
+            get
+            {
+                return pages["Map"];
+            }
+            set
+            {
+                pages["Map"] = value;
+                RaisePropertyChanged();
+            }
+        }
+        
+        public bool IsFavouritesVisible
+        {
+            get
+            {
+                return pages["Favourites"];
+            }
+            set
+            {
+                pages["Favourites"] = value;
+                RaisePropertyChanged();
+            }
+        }
+        
+        public bool IsHistoryVisible
+        {
+            get
+            {
+                return pages["History"];
+            }
+            set
+            {
+                pages["History"] = value;
+                RaisePropertyChanged();
+            }
+        }
+        
+        public bool IsSettingsVisible
+        {
+            get
+            {
+                return pages["Settings"];
+            }
+            set
+            {
+                pages["Settings"] = value;
                 RaisePropertyChanged();
             }
         }
--- src/TramlineFive/TramlineFive/TramlineFive/MasterPage.xaml.cs
+++ src/TramlineFive/TramlineFive/TramlineFive/MasterPage.xaml.cs
@@ -17,11 +17,7 @@ namespace TramlineFive
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MasterPage : ContentPage
     {
-        private MapPage mapPage = new MapPage();
-        private VirtualTablesPage vtPage = new VirtualTablesPage();
-        private HistoryPage historyPage = new HistoryPage();
-        private FavouritesPage favouritesPage = new FavouritesPage();
-
+        private bool appeared;
         private View Current => content.Children[0];
         public View View => content;
         public IGridList<View> Children => content.Children;
@@ -36,7 +32,6 @@ namespace TramlineFive
             set
             {
                 currentPage = value;
-                ChangeCurrentPage(value);
             }
         }
 
@@ -44,64 +39,28 @@ namespace TramlineFive
         {
             InitializeComponent();
 
-            vtPage.IsVisible = false;
-            favouritesPage.IsVisible = false;
-            historyPage.IsVisible = false;
-
-            content.Children.Add(mapPage);
-            content.Children.Add(vtPage);
-            content.Children.Add(favouritesPage);
-            content.Children.Add(historyPage);
-
             Messenger.Default.Register<FocusSearchMessage>(this, (m) =>
             {
                 search.Focus();
             });
         }
 
-        bool appeared;
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            await mapPage.OnAppearing();
             if (!appeared)
             {
+                Task[] loadingTasks = new Task[]
+                {
+                    mapPage.OnAppearing(),
+                    SimpleIoc.Default.GetInstance<VirtualTablesViewModel>().CheckForUpdatesAsync(),
+                    SimpleIoc.Default.GetInstance<HistoryViewModel>().LoadHistoryAsync(),
+                    SimpleIoc.Default.GetInstance<FavouritesViewModel>().LoadFavouritesAsync()
+                };
+
                 appeared = true;
-
-                await SimpleIoc.Default.GetInstance<VirtualTablesViewModel>().CheckForUpdatesAsync();
-                await SimpleIoc.Default.GetInstance<HistoryViewModel>().LoadHistoryAsync();
-                await SimpleIoc.Default.GetInstance<FavouritesViewModel>().LoadFavouritesAsync();
+                await Task.WhenAll(loadingTasks);
             }
-        }
-
-        private void mapClicked(object sender, EventArgs e)
-        {
-            ChangeCurrentPage(mapPage);
-        }
-
-        private void ChangeCurrentPage(View page)
-        {
-            foreach (View child in content.Children)
-            {
-                if (child.Equals(page))
-                    child.IsVisible = true;
-                else
-                    child.IsVisible = false;
-            }
-        }
-
-
-        private void vtClicked(object sender, EventArgs e)
-        {
-            ChangeCurrentPage(vtPage);
-        }
-        private void favClicked(object sender, EventArgs e)
-        {
-            ChangeCurrentPage(favouritesPage);
-        }
-        private void hClicked(object sender, EventArgs e)
-        {
-            ChangeCurrentPage(historyPage);
         }
     }
 }
