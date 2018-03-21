f99c95fa0275bf365931ba23341a5f3d317ce483
angelinn <angelin.nedelchev@outlook.com>
2018-03-21 23:15:46 +0200

    Add bool to color converter.

---
+++ src/TramlineFive/TramlineFive/TramlineFive/Converters/BoolToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TramlineFive.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        private string selectedColor = "#4CACE8";
+        private string notSelectedColor = "#808080";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Color.FromHex(selectedColor);
+
+            return Color.FromHex(notSelectedColor);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
