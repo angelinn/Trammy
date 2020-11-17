0127933b060734f0c5936d6b57f5fa811532de53
angelinn <angelin.nedelchev@outlook.com>
2020-11-17 11:17:52 +0200

    Update notifications to api 29.

--- src/TramlineFive/TramlineFive/TramlineFive.Android/Services/AlarmReceiver.cs
+++ src/TramlineFive/TramlineFive/TramlineFive.Android/Services/AlarmReceiver.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Util;
@@ -21,6 +22,8 @@ namespace TramlineFive.Droid.Services
     [IntentFilter(new string[] { "com.company.BROADCAST" })]
     public class AlarmReceiver : BroadcastReceiver
     {
+        private const string CHANNEL_ID = "tram";
+
         public override async void OnReceive(Context context, Intent intent)
         {
             try
@@ -41,18 +44,31 @@ namespace TramlineFive.Droid.Services
                     notificationIntent.SetData(Android.Net.Uri.Parse(version.ReleaseUrl));
                     PendingIntent pending = PendingIntent.GetActivity(context, 0, notificationIntent, PendingIntentFlags.CancelCurrent);
 
+                    NotificationManager manager = NotificationManager.FromContext(context);
+
+                    if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+                    {
+                        NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "My Notifications", NotificationImportance.Max);
+
+                        // Configure the notification channel.
+                        notificationChannel.Description = "Channel description";
+                        notificationChannel.EnableLights(true);
+                        notificationChannel.LightColor = Color.AliceBlue;
+                        notificationChannel.SetVibrationPattern(new long[] { 0, 1000, 500, 1000 });
+                        notificationChannel.EnableVibration(true);
+                        manager.CreateNotificationChannel(notificationChannel);
+                    }
+
                     Notification.Builder builder =
-                        new Notification.Builder(context)
+                        new Notification.Builder(context, CHANNEL_ID)
                             .SetContentTitle(title)
                             .SetContentText(message)
-                            .SetSmallIcon(Resource.Drawable.icon)
-                            .SetDefaults(NotificationDefaults.All)
-                            .SetPriority((int)Notification.PriorityHigh);
+                            .SetSmallIcon(Resource.Drawable.icon); 
 
                     builder.SetContentIntent(pending);
 
-                    Notification notification = builder.Build();
-                    NotificationManager manager = NotificationManager.FromContext(context);
+                    Notification notification = builder.Build(); 
+
                     manager.Notify(1337, notification);
                 }
             }
