d3004ff1a9da9de10ce39d0b2c2fa80c0028589c
angelinn <angelin.nedelchev@outlook.com>
2022-05-03 11:56:24 +0300

    Implement theming. Support settings/about screens.

---
+++ src/TramlineFive/TramlineFive/TramlineFive/Themes/DarkTheme.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TramlineFive.Themes;
+
+public partial class DarkTheme : ResourceDictionary
+{
+    public DarkTheme()
+    {
+        InitializeComponent();
+    }
+}
