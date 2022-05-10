e6d7f0c1d60b142fb47356835a53dfab1d4741a4
angelinn <angelin.nedelchev@outlook.com>
2022-05-10 10:21:56 +0300

    Improve dark theme colors.

--- src/TramlineFive/TramlineFive/TramlineFive.Android/MainActivity.cs
+++ src/TramlineFive/TramlineFive/TramlineFive.Android/MainActivity.cs
@@ -71,7 +71,7 @@ namespace TramlineFive.Droid
 
         private void OnThemeChanged(ChangeThemeMessage message)
         {
-            Android.Graphics.Color color = message.Name == "Light" ? Android.Graphics.Color.White : Android.Graphics.Color.ParseColor("#2d333b");
+            Android.Graphics.Color color = message.Name == "Light" ? Android.Graphics.Color.White : Android.Graphics.Color.ParseColor("#22272e");
             if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
                 Window.SetNavigationBarColor(color);
         }
