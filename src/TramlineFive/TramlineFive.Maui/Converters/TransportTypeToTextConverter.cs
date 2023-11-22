8d5d362ef1a894361be2be446f51530834d1cef3
angelinn <angelin.nedelchev@outlook.com>
2023-11-22 13:33:21 +0200

    Translate name of transport.

---
+++ src/TramlineFive/TramlineFive.Maui/Converters/TransportTypeToTextConverter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.ApplicationModel;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+
+namespace TramlineFive.Converters;
+
+public class TransportTypeToTextConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (value as string) switch
+        {
+            "bus" => "Автобус",
+            "tram" => "Твамвай",
+            "trolley" => "Тролей",
+            _ => "Транспорт",
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
