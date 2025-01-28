76159094ee172dccd0afc7fef2972239b6b84c4e
angelinn <angelin.nedelchev@outlook.com>
2025-01-28 13:23:49 +0200

    Properly focus search bar.

--- src/TramlineFive/TramlineFive.Maui/Views/SearchBar.xaml.cs
+++ src/TramlineFive/TramlineFive.Maui/Views/SearchBar.xaml.cs
@@ -1,4 +1,7 @@
 ﻿
+using CommunityToolkit.Mvvm.Messaging;
+using TramlineFive.Common.Messages;
+
 namespace TramlineFive.Maui.Views;
 
 public partial class SearchBar : Border
@@ -6,5 +9,10 @@ public partial class SearchBar : Border
     public SearchBar()
     {
         InitializeComponent();
+        WeakReferenceMessenger.Default.Register<StopSelectedMessage>(this, (r, m) =>
+        {
+            txtSearch.IsEnabled = false;
+            txtSearch.IsEnabled = true;
+        });
     }
 }
