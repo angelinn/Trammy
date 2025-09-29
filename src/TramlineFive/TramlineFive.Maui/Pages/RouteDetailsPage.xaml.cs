152602ad404cca35261368c08a63ca3780d0b49a
angelinn <angelin.nedelchev@outlook.com>
2025-09-29 13:53:14 +0300

    Improve route page design.

--- src/TramlineFive/TramlineFive.Maui/Pages/RouteDetailsPage.xaml.cs
+++ src/TramlineFive/TramlineFive.Maui/Pages/RouteDetailsPage.xaml.cs
@@ -14,9 +14,9 @@ public partial class RouteDetailsPage : ContentPage, IQueryAttributable
 
     public async void ApplyQueryAttributes(IDictionary<string, object> query)
     {
-		if (query.TryGetValue("Arrival", out object arrival) && query.TryGetValue("stopCode", out object stopCode))
+		if (query.TryGetValue("Arrival", out object arrival) && query.TryGetValue("stopCode", out object stopCode) && query.TryGetValue("stopName", out object stopName))
 		{
-			await (BindingContext as RouteDetailViewModel).LoadAsync(arrival as RouteArrivalInformation, stopCode as string);
+			await (BindingContext as RouteDetailViewModel).LoadAsync(arrival as RouteArrivalInformation, stopCode as string, stopName as string);
 		}
     }
 }
